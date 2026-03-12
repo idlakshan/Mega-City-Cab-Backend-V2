25d8275d3d19e802affb539635a557cbbe5e86fb
idlakshan <id.lakshan21@gmail.com>
2026-03-12 18:14:58 +0530

    initial project

---
+++ src/test/java/lk/icbt/megacity/MegacityApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.icbt.megacity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegacityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
